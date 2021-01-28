9dd502e6223fb087825753b6e1e9d8b2a2cb9a32
Paseul <machianb@naver.com>
2021-01-28 11:00:18 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import cv2
+import sys
+import time
+import urllib.request
+from collections import deque
+from onvif import ONVIFCamera
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
